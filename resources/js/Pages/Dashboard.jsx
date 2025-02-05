c2e43c8c9523ee68284a5b4f1e5ef4a8c24f4291
faratim <faratim@gmail.com>
2025-02-05 09:35:31 -0500

    building out the dashboard a bit

--- resources/js/Pages/Dashboard.jsx
+++ resources/js/Pages/Dashboard.jsx
@@ -1,7 +1,9 @@
-import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout';
-import { Head } from '@inertiajs/react';
+import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout";
+import { Head, Link, usePage } from "@inertiajs/react";
 
 export default function Dashboard() {
+    const { auth } = usePage().props;
+
     return (
         <AuthenticatedLayout
             header={
@@ -16,8 +18,30 @@ export default function Dashboard() {
                 <div className="mx-auto max-w-7xl sm:px-6 lg:px-8">
                     <div className="overflow-hidden bg-white shadow-sm sm:rounded-lg">
                         <div className="p-6 text-gray-900">
-                            You're logged in!
+                            Hello, {auth.user.name}!
                         </div>
+                        <Link
+                            href="/clients"
+                            className={
+                                "block w-full px-4 py-2 text-start text-sm leading-5 text-gray-700 transition duration-150 ease-in-out hover:bg-gray-100 focus:bg-gray-100 focus:outline-none "
+                            }
+                        >
+                            Manage Clients
+                        </Link>
+                        <Link
+                            className={
+                                "block w-full px-4 py-2 text-start text-sm leading-5 text-gray-700 transition duration-150 ease-in-out hover:bg-gray-100 focus:bg-gray-100 focus:outline-none "
+                            }
+                        >
+                            Refer a Client to a Coach
+                        </Link>
+                        <Link
+                            className={
+                                "block w-full px-4 py-2 text-start text-sm leading-5 text-gray-700 transition duration-150 ease-in-out hover:bg-gray-100 focus:bg-gray-100 focus:outline-none "
+                            }
+                        >
+                            See Your Referrals
+                        </Link>
                     </div>
                 </div>
             </div>
